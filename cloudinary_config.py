7afb0c10c487b5f6d0f58b5d991e7cf8990043cc
kasaburi <natiakasaburi2001w@gmail.com>
2026-07-16 15:56:21 +0400

    Fix Cloudinary config

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -8,9 +8,15 @@ CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
 CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
 CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
-print("Cloudinary Cloud Name:", CLOUDINARY_CLOUD_NAME)
-print("Cloudinary API Key exists:", bool(CLOUDINARY_API_KEY))
-print("Cloudinary Secret exists:", bool(CLOUDINARY_API_SECRET))
+if not CLOUDINARY_CLOUD_NAME:
+    raise ValueError("CLOUDINARY_CLOUD_NAME is missing")
+
+if not CLOUDINARY_API_KEY:
+    raise ValueError("CLOUDINARY_API_KEY is missing")
+
+if not CLOUDINARY_API_SECRET:
+    raise ValueError("CLOUDINARY_API_SECRET is missing")
+
 
 cloudinary.config(
     cloud_name=CLOUDINARY_CLOUD_NAME,
