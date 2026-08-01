bf598896a7641ec5277c6f930f8e13d12694c545
kasaburi <natiakasaburi2001w@gmail.com>
2026-08-01 20:45:04 +0400

    Add global exception handler

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import JSONResponse
+import traceback
 from routers.routers.cities import router as cities_router
 from routers.routers.categories import router as categories_router
 from routers.routers.reports import router as reports_router
@@ -8,14 +9,27 @@ from routers.routers.auth import router as auth_router
 from routers.routers import statistics
 
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI(
     title="Fix Georgia API"
 )
 
 
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    traceback.print_exc()
 
-
+    return JSONResponse(
+        status_code=500,
+        content={
+            "message": "Internal Server Error",
+            "error": str(exc)
+        }
+    )
 
 
 
